a20e37ae822c2df9ede74bffeb09d5635cc79a23
Tyler Hawkins <thawkin3@byu.net>
2022-11-06 21:21:30 -0700

    feat: adds completed request form features

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css'
+import { RequestForm } from './RequestForm'
 
 export const App = () => {
   return (
     <main>
       <h1>TDD for Building UIs Demo</h1>
+      <RequestForm />
     </main>
   )
 }
